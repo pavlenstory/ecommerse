051d8a686dccb86f6dfa8e882e33bd187d0ea0bb
Pavel Andreikovets <potto8989@mail.ru>
2022-10-19 18:16:26 +0300

    feat: add pagination, improve sorting

--- my-app/src/components/ProductList/ProductList.tsx
+++ my-app/src/components/ProductList/ProductList.tsx
@@ -1,11 +1,15 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 import { MainReducerState } from '../../interfaces'
+import Pagination from '@mui/material/Pagination';
+
+const PAGE_LIMIT: number = 4
 
 type Props = Partial<MainReducerState> & { fetchProductList: Function }
 
 export const ProductList = (props: Props) => {
-    const { fetchProductList, products, searchString } = props;
+    const { fetchProductList, products = [], searchString, currency = 'USD' } = props;
     const [sortedProducts, setSortedProducts] = useState(products)
+    const [currentPage, setCurrentPage] = useState<number>(1)
 
     useEffect(() => {
         fetchProductList()
@@ -13,17 +17,30 @@ export const ProductList = (props: Props) => {
 
     useEffect(() => {
         if (searchString) {
-            setSortedProducts(products?.filter(e => (e.description + e.price + e.title).includes(searchString)))
+            setSortedProducts(products?.filter(e => (
+                e.description + e.title).toLowerCase().includes(searchString.toLowerCase()
+                )))
         } else {
             setSortedProducts(products)
         }
     }, [searchString, products])
 
+    const { pageLimit, pageStart, pageEnd } = useMemo(() => {
+        const pageLimit = Math.ceil(sortedProducts?.length / PAGE_LIMIT)
+        const pageStart = (currentPage - 1) * PAGE_LIMIT
+        const pageEnd = pageStart + PAGE_LIMIT
+        return { pageLimit, pageStart, pageEnd }
+    }, [currentPage, sortedProducts?.length])
+
     return (
-        <div>
-            {sortedProducts?.map(p => (
-                <div key={p.id}>{p.title} - {p.description} - {p.price}</div>
+        <>
+            {sortedProducts?.slice(pageStart, pageEnd).map(p => (
+                <div>
+                    <img src={p.image} />
+                    <div key={p.id}>{p.title} - {p.description} - {p.price}</div>
+                </div>
             ))}
-        </div>
+            <Pagination count={pageLimit} page={currentPage} onChange={(_, num) => setCurrentPage(num)} />
+        </>
     )
 }
